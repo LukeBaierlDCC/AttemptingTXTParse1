3be6e66876869560357951feb2616cddaa123229
LUKE BAIERL <lukeest1984@gmail.com>
2021-08-31 18:45:14 -0500

    First commit

---
+++ ParseCSVAttempt1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseCSVAttempt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string rawCSV = System.IO.File.ReadAllText(@"C:\Users\lukee\Documents\CSV Files\videogameparse.csv");
+            string[] csvLines = System.IO.File.ReadAllLines(@"C:\Users\lukee\Documents\CSV Files\videogameparse.txt");
+            
+            for (int i = 0; i < csvLines.Length; i++)
+            {
+                //Console.WriteLine(csvLines[i]);
+
+                string[] rowData = csvLines[i].ToUpper().Split(',', ' ');
+                for (int j = 0; j < rowData.Length; j++)
+                {
+                    Console.WriteLine(rowData[j]);
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
